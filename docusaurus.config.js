7b3840b48ff35c2f70ac7656bebb494f3c29a6d6
SOUCHAL Martin <martin.souchal@inrae.fr>
2024-10-18 15:28:58 +0200

    actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://csan-hub.github.io/csan-doc/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/csan-hub/',
+  baseUrl: '/csan-doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
