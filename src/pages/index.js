34cb0175d221e5255215378314803ca36cc671e8
SOUCHAL Martin <martin.souchal@inrae.fr>
2024-11-25 11:45:07 +0100

    fix domain

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Je découvre !
+            Je commence a partager mes conteneurs !
           </Link>
         </div>
       </div>
